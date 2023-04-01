a44d37a8aa26ce89be145651a34a083fa5a863fc
Aljaz Medic <aljaz.medic@gmail.com>
2023-04-01 14:58:47 +0200

    Add socket communication

--- challs/singing_rsa/app/app.py
+++ challs/singing_rsa/app/app.py
@@ -1,7 +1,9 @@
+#!/usr/bin/env python3
 from Crypto.Util.number import long_to_bytes, bytes_to_long, getPrime, inverse
 import os
-import socket
-import threading
+import sys
+
+
 
 FLAG = os.environ.get('FLAG', 'flag{test_flag}')
 MARKOV_PODPIS = b'To je poslal Marko.'
@@ -18,11 +20,12 @@ class CryptoSession:
 
     def sign_message(self, sporocilo: bytes):
         if MARKOV_PODPIS in sporocilo:
-            return None, 'Ti pa nisi Mark!'
+            return None, b'Ti pa nisi Marko!'
         sporocilo = bytes_to_long(sporocilo)
         if sporocilo > self.n:
-            return None, 'Predolgo sporočilo!'
-        return pow(sporocilo, self.d, self.n), None
+            return None, b'Predolgo sporocilo!'
+        podpis = pow(sporocilo, self.d, self.n)
+        return (podpis, self.e, self.n), None
 
     def verify(self, sporocilo: bytes, podpis: int) -> bool:
         sporocilo = bytes_to_long(sporocilo)
@@ -30,64 +33,25 @@ class CryptoSession:
             return False
         return pow(podpis, self.e, self.n) == sporocilo
 
-    def get_public_key(self) -> tuple:
-        return self.n, self.e
-
-    def get_flag(self, m: str, s: int) -> str:
+    def get_flag(self, m: str, s: int):
         if MARKOV_PODPIS in m and self.verify(m, s):
             return FLAG, None
-        return None, 'Napačen podpis!'
+        return None, b'Napacen podpis!'
 
 
 class Connection:
-    def __init__(self, conn: socket) -> None:
+    def __init__(self) -> None:
         self.blackbox = CryptoSession()
-        self.conn = conn
-
-    def recvuntil(self, delim: bytes = b'\n') -> bytes:
-        b = b''
-        chunk_size = 1024
-        while True:
-            _chunk = self.conn.recv(chunk_size)
-            if len(_chunk) < chunk_size:
-                b += _chunk
-                break
-            if delim in _chunk:
-                b += _chunk[:_chunk.index(delim) + 1]
-                break
-            b += _chunk
-        return b
-
-    def send(self, data: bytes) -> None:
-        if isinstance(data, str):
-            data = data.encode('utf-8')
-        self.conn.sendall(data)
-
-    # Just some misc methods
-    def recvline(self) -> bytes:
-        return self.recvuntil(b'\n')
-
-    def recvint(self) -> int:
-        return int(self.recvline())
-
-    def prompt(self, prompt: bytes) -> bytes:
-        self.send(prompt)
-        return self.recvline()
-
-    def prompt_int(self, prompt: bytes) -> int:
-        self.send(prompt)
-        return self.recvint()
-    # Ok that's enough misc methods
-
+    
     def run(self):
         menu = b'''\
-[1] Podpisi sporocilo zase!
-[2] Jaz sem Marko, preveri moj podpis!
+[1] Podpisi sporocilo zase
+[2] Jaz sem Marko, preveri moj podpis
 [3] Izhod
 Izbira: '''
         while True:
             try:
-                option = self.prompt_int(menu)
+                option = int(self.input(menu).decode())
                 if option == 1:
                     self.sign_prompt()
                 elif option == 2:
@@ -96,45 +60,57 @@ Izbira: '''
                     self.exit()
                     break
                 else:
-                    self.send('Neveljavna izbira!')
-            except (ValueError, UnicodeDecodeError):
-                pass
+                    print(b'Neveljavna izbira!')
+            except (EOFError) as e:
+                self.print(b'Napaka!')
+                pass    
+            except (UnicodeError) as e:
+                self.print("UnicodeError")
+                raise e
+            self.print()
 
     def sign_prompt(self):
-        t = self.prompt("Sporocilo").encode('utf-8')
+        t = self.input(b"Sporocilo: ")
+
         data, err = self.blackbox.sign_message(t)
         if err:
-            return self.send(b'Napaka pri podpisovanju!\n')
-        self.send(f'Podpis: {data}')
+            self.print(b'Napaka pri podpisovanju:', err)
+            return 
+        p,e,n = data
+        self.print(b'Podpis:', f"p={p}", f"e={e}", f"n={n}", sep='\n')
 
     def verify_prompt(self):
-        t = self.prompt('Sporočilo: ').encode('utf-8')
-        s = self.prompt_int('Podpis: ')
-        msg, err = self.blackbox.verify_signature(t, s)
+        t = self.input(b'Sporocilo: ')
+        s = int(self.input(b'Podpis: ').decode())
+        msg, err = self.blackbox.get_flag(t, s)
         if err:
-            return self.send(b'Napaka pri preverjanju podpisa!\n')
-        self.send(f'Podpis je veljaven!\n{msg}')
+            self.print(f'Napaka pri preverjanju podpisa: {err}')
+            return
+        self.print(f'Podpis je veljaven!\n{msg}')
 
     def exit(self):
-        self.send('Nasvidenje!\n')
-        self.conn.close()
+        self.print(b'Nasvidenje!')
+        exit(0)
+    
+    ### Socket stuff
+    def to_bytes(self, x):
+        if isinstance(x, bytes):
+            return x
+        if isinstance(x, str):
+            return x.encode()
+        return str(x).encode()
+
+    def print(self, *args, sep=' ', end='\n'):
+        args = map(self.to_bytes, args)
+        sep, end = map(self.to_bytes, (sep, end))
+        sys.stdout.buffer.write(sep.join(args) + end)
+        sys.stdout.buffer.flush()
+
+    def input(self, prompt):
+        self.print(prompt, end='')
+        return sys.stdin.buffer.readline().strip()
 
 
 if __name__ == '__main__':
-    # run socket server and wait for connections
-    HOST = os.environ.get('HOST', 'localhost')
-    PORT = int(os.environ.get('PORT', 1337))
-
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind((HOST, PORT))
-    s.listen(1)
-    s.settimeout(3)
-    print(f'Listening on {HOST}:{PORT}')
-    while True:
-        try:
-            conn, addr = s.accept()
-            print(f'Connected by {addr}')
-            t = threading.Thread(target=Connection(conn).run)
-            t.start()
-        except socket.timeout:
-            pass
+    Connection().run()
+    
