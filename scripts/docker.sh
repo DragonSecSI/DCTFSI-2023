8caf074d2eecd7f4b4414f7c17b437882249152b
aJuvan <andraz.juvan@heimdall.si>
2023-04-11 16:25:39 +0200

    Added vun deployment and helper scripts

---
+++ scripts/docker.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -x
+
+dockerize() {
+	folder="challs/$1"
+	name="dctf23.azurecr.io/challs/$2:latest"
+	pushd $folder
+	docker build --platform=linux/amd64 -t $name . &&
+	docker push $name
+	popd
+}
+
+# Pwn
+dockerize "vun" "vun"
